c798afaccae9b5966795a3728b4e7689b7b5fd20
Mykhaylo (Misha) Ivanovych Kuzma <mishakuzma@users.noreply.github.com>
2020-03-28 17:16:43 -0400

    Upload Webmap

---
+++ data/EventParameters_1.js
@@ -0,0 +1,8 @@
+var json_EventParameters_1 = {
+"type": "FeatureCollection",
+"name": "EventParameters_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "2" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -80.513958210397831, 43.401326683968627 ], [ -80.514036930051006, 43.401359043979838 ], [ -80.514123935983463, 43.401232614070437 ], [ -80.514015178567888, 43.401201759112027 ], [ -80.513958210397831, 43.401326683968627 ] ] ] ] } }
+]
+}
